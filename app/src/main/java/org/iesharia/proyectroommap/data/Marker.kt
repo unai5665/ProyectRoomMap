ecdadf4708c6512561961c4f4423efca5f2e3ef3
unai <unaiperez5665@gmail.com>
2024-12-04 08:56:45 +0000

    creada tabla MarkerType

--- app/src/main/java/org/iesharia/proyectroommap/data/Marker.kt
+++ app/src/main/java/org/iesharia/proyectroommap/data/Marker.kt
@@ -1,2 +1,13 @@
 package org.iesharia.proyectroommap.data
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity
+data class MarkerType(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val name: String,
+    val iconResource: Int
+    )
+
