4f537ea6542e2dca0c8ad8e3deb37f1266c9cfb1
unai <unaiperez5665@gmail.com>
2024-12-10 10:00:01 +0000

    Inicialización de los DAOs

--- app/src/main/java/org/iesharia/proyectroommap/model/MarkerViewModel.kt
+++ app/src/main/java/org/iesharia/proyectroommap/model/MarkerViewModel.kt
@@ -1,2 +1,15 @@
 package org.iesharia.proyectroommap.model
 
+import android.app.*
+import androidx.lifecycle.*
+import kotlinx.coroutines.*
+import org.iesharia.proyectroommap.data.AppDatabase
+import org.iesharia.proyectroommap.data.Marker
+import org.iesharia.proyectroommap.data.MarkerType
+
+
+class MarkerViewModel(application: Application) : AndroidViewModel(application) {
+    private val markerDao = AppDatabase.getDatabase(application).markerDao()
+    private val markerTypeDao = AppDatabase.getDatabase(application).markerTypeDao()
+
+}
