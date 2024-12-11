dbc1749185593ca262150d97bbca647e04a92aeb
unai5665 <unaiperez5665@gmail.com>
2024-12-11 20:55:57 +0000

    creado MyMapView.kt

---
+++ app/src/main/java/org/iesharia/proyectroommap/view/MyMapView.kt
@@ -0,0 +1,49 @@
+package org.iesharia.proyectroommap.view
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.example.viewmodelrm.R
+import com.utsman.osmandcompose.DefaultMapProperties
+import com.utsman.osmandcompose.Marker
+import com.utsman.osmandcompose.OpenStreetMap
+import com.utsman.osmandcompose.ZoomButtonVisibility
+import com.utsman.osmandcompose.rememberCameraState
+import com.utsman.osmandcompose.rememberMarkerState
+import org.iesharia.proyectroommap.model.MarkerViewModel
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
+import org.osmdroid.tileprovider.tilesource.XYTileSource
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.util.MapTileIndex
+
+val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
+    "Google-Sat",
+    0, 19, 256, ".png", arrayOf(
+        "https://mt0.google.com",
+        "https://mt1.google.com",
+        "https://mt2.google.com",
+        "https://mt3.google.com"
+    )
+) {
+    override fun getTileURLString(aTile: Long): String {
+        return baseUrl + "/vt/lyrs=s&x=" + MapTileIndex.getX(aTile) + "&y=" + MapTileIndex.getY(
+            aTile) + "&z=" + MapTileIndex.getZoom(aTile)
+    }
+}
+
