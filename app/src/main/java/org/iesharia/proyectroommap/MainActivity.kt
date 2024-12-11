5642543a8780ec1cb0e996afda0032b63aa9bd4b
unai5665 <unaiperez5665@gmail.com>
2024-12-11 20:50:37 +0000

    añadida llamadad para inicializar los datos del MarkerViewModel

--- app/src/main/java/org/iesharia/proyectroommap/MainActivity.kt
+++ app/src/main/java/org/iesharia/proyectroommap/MainActivity.kt
@@ -1,33 +1,30 @@
-package org.iesharia.proyectroommap
+package com.example.proyectroommap
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import org.iesharia.proyectroommap.data.AppDatabase
-import org.iesharia.proyectroommap.view.MarcadoresViewModel
+import org.iesharia.proyectroommap.model.MarkerViewModel
 import org.iesharia.proyectroommap.view.MyMapView
 
-
 class MainActivity : ComponentActivity() {
-    private val viewModel: MarcadoresViewModel by viewModels()
+    private val viewModel: MarkerViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Cargar marcadores al iniciar la actividad
-        viewModel.cargarMarcadores()
+       
+        viewModel.initializeData()
 
         setContent {
             MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    // Mostrar el mapa y los marcadores
                     MyMapView(
                         modifier = Modifier.padding(innerPadding),
                         viewModel = viewModel
