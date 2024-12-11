1132b7b43953b7c43d82993be8412eefa96a97ff
unai5665 <unaiperez5665@gmail.com>
2024-12-11 21:42:11 +0000

    añadidas funciones para no crear datos duplicados

--- app/src/main/java/org/iesharia/proyectroommap/data/DaoMarker.kt
+++ app/src/main/java/org/iesharia/proyectroommap/data/DaoMarker.kt
@@ -12,6 +12,12 @@ interface DaoMarker {
     @Insert(onConflict = OnConflictStrategy.IGNORE)  // Insertará solo si no existe un marcador con esas coordenadas
     suspend fun insertMarker(marker: Marker)
 
+    @Query("DELETE FROM Marker WHERE id NOT IN (SELECT MIN(id) FROM Marker GROUP BY latitude, longitude)")  // Elimina duplicados
+    suspend fun deleteDuplicateMarkers()
+
+
+    @Query("SELECT * FROM Marker")
+    fun getAllMarkersSync(): List<Marker>
 }
 
 
@@ -23,4 +29,8 @@ interface MarkerTypeDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)  // Insertará solo si no existe ya un tipo igual
     suspend fun insertMarkerType(markerType: MarkerType)
 
+    @Query("DELETE FROM MarkerType WHERE id NOT IN (SELECT MIN(id) FROM MarkerType GROUP BY name)")  // Elimina duplicados
+    suspend fun deleteDuplicateMarkerTypes()
+    @Query("SELECT * FROM MarkerType")
+    fun getAllMarkerTypesSync(): List<MarkerType>
 }
